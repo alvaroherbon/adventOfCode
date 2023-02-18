4fef342636919a717b6668e4adfccc2946b55a02
alvaroherbon <57678071+alvaroherbon@users.noreply.github.com>
2023-02-18 16:21:19 +0100

    added 4

---
+++ 2022/4/1.py
@@ -0,0 +1,20 @@
+file = open("input.txt", "r")
+input = file.read()
+
+def main(): 
+    number = 0
+    for line in input.splitlines(): 
+        condition = False
+        Onesub1 = int(line.split(",")[0].split("-")[0].strip())
+        Onesub2 = int(line.split(",")[0].split("-")[1].strip())
+        Twosub1 = int(line.split(",")[1].split("-")[0].strip())
+        Twosub2 = int(line.split(",")[1].split("-")[1].strip())
+        
+        if (Onesub1 <= Twosub1 and Onesub2 >= Twosub2) or (Twosub1 <= Onesub1 and Twosub2 >= Onesub2):
+            condition = True
+            number += 1
+        print(condition)
+    print(number)
+         
+main()
+
