e420c85081dcd36bbecdf053d3e220cd62423ad1
alvaroherbon <57678071+alvaroherbon@users.noreply.github.com>
2023-02-20 22:29:14 +0100

    minor changes

--- 2022/5/1.py
+++ 2022/5/1.py
@@ -1,16 +1,23 @@
 file = open("input.txt", "r")
 input = file.read()
 
-def main(): 
-    grid = []
-    actions = []
-    for line in input.splitlines()[:8]:
-        grid.append(line)
-    for line in input.splitlines()[10:]:
-        crates = line.split(" ")[1]
-        fr = line.split(" ")[3]
-        to = line.split(" ")[5]
-        for i in range(int(crates)):
-            
 
-main() 
+def main():
+    grid = [[], [], [], [], [], [], [], [], []]
+    actions = []
+    for i in reversed(range(8)):
+        numRow = 0
+        for j in range(0, len(input.splitlines()[i]), 4):
+            char = input.splitlines()[i][j+1]
+            if char != " " and char != "[" and char != "]":
+                grid[numRow].append(char)
+            numRow += 1
+    print(grid)
+    # for line in input.splitlines()[10:]:
+    #     crates = line.split(" ")[1]
+    #     fr = line.split(" ")[3]
+    #     to = line.split(" ")[5]
+    #     #print(crates)
+
+
+main()
